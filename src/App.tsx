d53289d05f2a0cbf37189a90ef76b6b8f552d143
Jonathan Dajome R. <jonathandajome@gmail.com>
2023-10-31 15:25:42 -0500

    updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,15 @@
-import { useState } from "react";
-import { Modal, TaskList } from "./components";
-import { useChangeInput, useShowMenu } from "./hooks";
-import { getLimit } from "./utils/getLimit";
-
-interface Tasks {
-  id: number;
-  name: string;
-}
-
-let nextID = 0;
-
 const App = () => {
-  const [tasks, setTasks] = useState<Tasks[]>([]);
-  const { isModalOpen, toggleMenu } = useShowMenu();
-  const { name, handleChangeName, clearInput } = useChangeInput();
-
-  const handleClick = () => {
-    if (!name) {
-      toggleMenu();
-      return;
-    }
-
-    const upperCaseName = name.charAt(0).toUpperCase() + name.slice(1);
-    const value = getLimit(upperCaseName);
-
-    setTasks([{ id: nextID++, name: value }, ...tasks]);
-    clearInput();
-  };
-
-  const handleRemove = (id: number) => {
-    setTasks(tasks.filter((artist) => artist.id !== id));
-  };
-
   return (
-    <>
-      {isModalOpen && <Modal isOpen={isModalOpen} showModal={toggleMenu} />}
-      <div className="container">
-        <h1 className="container__title">Todos</h1>
-        <div className="container__task">
-          <input
-            type="text"
-            value={name}
-            placeholder="create new task"
-            onChange={handleChangeName}
-          />
-
-          <div className="container__list">
-            <ul>
-              {tasks.map(({ id, name }) => (
-                <TaskList
-                  key={id}
-                  item={name}
-                  onClick={() => handleRemove(id)}
-                  text="Delete"
-                />
-              ))}
-            </ul>
-          </div>
-        </div>
-        <button type="button" onClick={handleClick}></button>
-      </div>
-    </>
+    <div className="container">
+      <section className="container__tasks">
+        <h1>Todo</h1>
+        <input type="text" />
+        <button>Add</button>
+      </section>
+      <section className="container__todo-list">
+        <ul>Todo list</ul>
+      </section>
+    </div>
   );
 };
 
