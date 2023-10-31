56e384a60bdfc9b4a110c328a2e7a0fce81cf1d4
Jonathan Dajome R. <jonathandajome@gmail.com>
2023-10-31 16:11:52 -0500

    add model file

---
+++ src/model.ts
@@ -0,0 +1,11 @@
+interface TasksProps {
+  id: `${string}-${string}-${string}-${string}-${string}`;
+  text: string;
+  done: boolean;
+}
+
+const initialTasks: TasksProps[] = [
+  { id: crypto.randomUUID(), text: "Visit the Kafka Museum", done: true },
+  { id: crypto.randomUUID(), text: "See puppet show", done: false },
+  { id: crypto.randomUUID(), text: "Lennon wall photo", done: false },
+];
