1aa6ae1739bb1eae56d25a9f28794bd933ddf3c8
Jonathan <jonathandajome@gmail.com>
2023-11-20 11:22:21 -0500

    updated app component: added localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from "react";
-import { TasksProps, initialTasks } from "./model";
+import { useEffect, useState } from "react";
+import { TasksProps } from "./model";
 import AddTask from "./components/AddTask";
 import { TaskList } from "./components";
 import TimedMessage from "./components/TimedMessage";
 
 const App = () => {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, setTasks] = useState<TasksProps[]>([]);
   const [error, setError] = useState<null | string>(null);
 
+  useEffect(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
   const handleAddTask = (text: string) => {
+    let textCapitalized =
+      text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
     if (!text) {
       setError(null);
       setTimeout(() => setError("Please enter a task"), 0);
     } else {
-      setTasks([...tasks, { id: crypto.randomUUID(), text, done: false }]);
+      setTasks([
+        ...tasks,
+        { id: crypto.randomUUID(), text: textCapitalized, done: false },
+      ]);
       setError(null);
     }
   };
 
   const handleChangeTask = (task: TasksProps) => {
-    setTasks(
-      tasks.map((t) => {
-        if (t.id === task.id) {
-          return task;
-        } else {
-          return t;
-        }
-      })
-    );
+    setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
   };
 
   const handleDeleteTask = (taskId: string) => {
