24ac204da39e3ab255b21129a704baf6fa9c1708
Jonathan <jonathandajome@gmail.com>
2024-11-13 15:33:12 -0500

    update layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import type { LayoutProps } from "../interface";
 import "./Layout.css";
+import { useTheme } from "../hooks/useTheme";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return <section className="layout">{children}</section>;
+  const { theme } = useTheme();
+
+  return (
+    <section className={`layout ${theme === "light" ? "dark" : "light"}`}>
+      {children}
+    </section>
+  );
 };
 
 export default Layout;
