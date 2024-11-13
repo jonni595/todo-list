6eba7bff3b57c38df81a25546672a29dc0fa296f
Jonathan dajome <jonathandajome@gmail.com>
2024-11-13 17:54:41 -0500

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
+import TaskList from "./components/TaskList";
 import Layout from "./layout/Layout";
+import { todoList } from "./data";
+import type { Tasks } from "./interface";
+import AddTask from "./components/AddTask";
 
 const App = () => {
+  const [tasks, setTasks] = useState<Tasks[]>(todoList);
+
+  const handleAddTask = (text: string) => {
+    if (!text) return;
+    setTasks([
+      ...tasks,
+      { id: crypto.randomUUID(), text: text.toLowerCase(), done: false },
+    ]);
+  };
+
+  const handleChangeTask = (task: Tasks) => {
+    setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
+  };
+
+  const handleDeleteTask = (id: string) => {
+    setTasks(tasks.filter((t) => t.id !== id));
+  };
+
   return (
     <Layout>
       <Navbar />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+      <AddTask onAddTask={handleAddTask} />
     </Layout>
   );
 };
