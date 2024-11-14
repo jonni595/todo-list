ef3e65f3f29c3c5bfb8831739c00b6679d54ea63
Jonathan <jonathandajome@gmail.com>
2024-11-14 08:36:19 -0500

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,12 @@ import { useTheme } from "../hooks/useTheme";
 
 const Navbar = () => {
   const [isDark, setIsDark] = useState(false);
-  const { toggleTheme } = useTheme();
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <header className="header">
       <button
+        style={{ color: theme === "light" ? "#fff" : "#000" }}
         className="btn"
         onClick={() => {
           setIsDark(!isDark);
