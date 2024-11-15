3700bd8c9192773cdffe6f35617111b0e36fe5a9
Jonathan <jonathandajome@gmail.com>
2024-11-14 21:46:00 -0500

    refactoring app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import { useState } from "react";
 import Navbar from "./components/Navbar";
 import TaskList from "./components/TaskList";
 import Layout from "./layout/Layout";
-import { todoList } from "./data";
-import type { Tasks } from "./interface";
 import AddTask from "./components/AddTask";
+import { useTask } from "./hooks/useTask";
 
 const App = () => {
-  const [tasks, setTasks] = useState<Tasks[]>(todoList);
-
-  const handleAddTask = (text: string) => {
-    if (!text) return;
-    setTasks([...tasks, { id: crypto.randomUUID(), text, done: false }]);
-  };
-
-  const handleChangeTask = (task: Tasks) => {
-    setTasks(tasks.map((t) => (t.id === task.id ? task : t)));
-  };
-
-  const handleDeleteTask = (id: string) => {
-    setTasks(tasks.filter((t) => t.id !== id));
-  };
+  const { tasks, handleAddTask, handleChangeTask, handleDeleteTask } =
+    useTask();
 
   return (
     <Layout>
