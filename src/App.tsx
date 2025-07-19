19351f05f33ceb43048fc028c4ce8b27e3600439
Jonathan <jonathandajome@gmail.com>
2025-07-19 18:09:17 -0500

    Add Title Tasks component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import TitleTasks from "./components/title-tasks";
+
 export default function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="max-w-7xl min-h-screen m-auto p-5 border border-l-zinc-800 border-r-zinc-800">
+      <TitleTasks count={5} />
     </div>
   );
 }
---
+++ src/components/title-tasks.tsx
@@ -0,0 +1,32 @@
+import clsx from "clsx";
+import { Badge } from "@/components/ui/badge";
+
+export default function TitleTasks({ count }: { count: number }) {
+  return (
+    <div
+      className={clsx(
+        "w-full h-28 sm:h-auto",
+        "flex flex-col items-center justify-around sm:flex-row sm:justify-between"
+      )}
+    >
+      <div>
+        <h1
+          className={clsx(
+            "font-semibold text-center sm:leading-12",
+            "text-xl md:text-3xl lg:text-4xl"
+          )}
+        >
+          Welcome!
+        </h1>
+        <h6
+          className={clsx("text-gray-400", "text-sm md:text-base lg:text-lg")}
+        >
+          Here's a list of your tasks.
+        </h6>
+      </div>
+      <Badge variant="default" className="sm:text-sm">
+        Total: {count}
+      </Badge>
+    </div>
+  );
+}
