bc866b2e063ec881650d96eac2a7416cb154fc1c
Jonathan <jonathandajome@gmail.com>
2025-07-19 15:50:46 -0500

    remove project

--- src/hooks/useTheme.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { Context } from "../context/Context";
-
-export const useTheme = () => {
-  if (!useContext(Context)) {
-    throw new Error("useTheme must be used within a ThemeProvider");
-  }
-  return useContext(Context);
-};
