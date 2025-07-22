204fa3a0d4a07a7d094bb454fdd52e4ab5aba5c4
Jonathan <jonathandajome@gmail.com>
2025-07-21 20:35:49 -0500

    Add TaskList

---
+++ src/components/TasksList.tsx
@@ -0,0 +1,24 @@
+import { type TaskListProps } from "../lib/definitions";
+import Task from "./Task";
+
+export default function TasksList({
+  tasks,
+  onChangeTasks,
+  onDeleteTask,
+}: TaskListProps) {
+  return (
+    <div className="w-full flex">
+      <ul className="w-full">
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <Task
+              task={task}
+              onChangeTask={onChangeTasks}
+              onDeleteTask={onDeleteTask}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
